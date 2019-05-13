c72a3506603c447d9dde6a09dd4aa6ec1c740385
Rony <ronymmoura@gmail.com>
2019-05-13 17:56:21 -0300

    Criando calendário de pagamento e processo de beneficio

---
+++ Intech.PrevSystem.Saofrancisco.API/Controllers/CalendarioPagamentoController.cs
@@ -0,0 +1,27 @@
+﻿#region Usings
+using Intech.PrevSystem.API;
+using Intech.PrevSystem.Negocio.Proxy;
+using Microsoft.AspNetCore.Mvc;
+using System; 
+#endregion
+
+namespace Intech.PrevSystem.Saofrancisco.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarioPagamentoController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Json(new CalendarioPagamentoProxy().Listar());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Intech.PrevSystem.Saofrancisco.API/Controllers/ProcessoBeneficioController.cs
@@ -0,0 +1,11 @@
+﻿using Intech.PrevSystem.API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intech.PrevSystem.Saofrancisco.API.Controllers
+{
+    [Route(RotasApi.ProcessoBeneficio)]
+    [ApiController]
+    public class ProcessoBeneficioController : BaseProcessoBeneficioController
+    {
+    }
+}
