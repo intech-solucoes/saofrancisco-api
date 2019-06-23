46a1ec6def5c5003e6435c69324a92e80362fd3e
Rony <ronymmoura@gmail.com>
2019-06-23 20:38:20 -0300

    Ajuste na rota de dependentes

--- Intech.PrevSystem.Saofrancisco.API/Controllers/DependenteController.cs
+++ Intech.PrevSystem.Saofrancisco.API/Controllers/DependenteController.cs
@@ -1,12 +1,30 @@
 ﻿#region Usings
 using Intech.PrevSystem.API;
+using Intech.PrevSystem.Negocio.Proxy;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 #endregion
 
 namespace Intech.PrevSystem.Saofrancisco.API.Controllers
 {
     [Route(RotasApi.Depentende)]
-    public class DependenteController : BaseDependenteController
+    public class DependenteController : BaseController
     {
+        [HttpGet]
+        [Authorize("Bearer")]
+        public IActionResult Get()
+        {
+            try
+            {
+                var funcionario = new FuncionarioProxy().BuscarPorCodEntid(CodEntid);
+
+                return Json(new DependenteProxy().BuscarPorFundacaoInscricao(funcionario.CD_FUNDACAO, funcionario.NUM_INSCRICAO));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
