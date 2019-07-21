2f257248230be835ed8f7f3da75c40ea94e76e56
Rony <ronymmoura@gmail.com>
2019-07-21 20:30:24 -0300

    Criando estrutura do LGPD

---
+++ Intech.PrevSystem.Saofrancisco.API/Controllers/LGPDController.cs
@@ -0,0 +1,58 @@
+﻿using Intech.PrevSystem.API;
+using Intech.PrevSystem.Entidades;
+using Intech.PrevSystem.Entidades.Constantes;
+using Intech.PrevSystem.Negocio.Proxy;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Intech.PrevSystem.Saofrancisco.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LGPDController : BaseController
+    {
+        [HttpGet]
+        [Authorize("Bearer")]
+        public IActionResult BuscarPorCPF()
+        {
+            try
+            {
+                return Json(new LGPDConsentimentoProxy().BuscarPorCPF(Cpf));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("{origem}")]
+        [Authorize("Bearer")]
+        public IActionResult Inserir(int origem)
+        {
+            try
+            {
+                var data = DateTime.Now;
+
+                var lgpd = new LGPDConsentimentoEntidade
+                {
+                    CD_FUNDACAO = CdFundacao,
+                    COD_IDENTIFICADOR = $"{data.ToString("ddMMyyyyhhmmss")}{Cpf}",
+                    COD_CPF = Cpf,
+                    DTA_CONSENTIMENTO = data,
+                    TXT_DISPOSITIVO = "",
+                    TXT_IPV4 = "",
+                    TXT_IPV6 = "",
+                    TXT_ORIGEM = DMN_SISTEMA_ORIGEM.Valor(origem)
+                };
+                new LGPDConsentimentoProxy().Inserir(lgpd);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
